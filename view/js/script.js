c89802ea38dfd220e3996fd61aa0523e7889874a
Jiye Choi <jiyechoi@Jiyes-MacBook-Air.local>
2019-12-05 02:26:03 -0500

    adding_rating

--- view/js/script.js
+++ view/js/script.js
@@ -2,13 +2,18 @@ $(document).ready(function () {
 
     $("#tabs").tabs();
 
-    console.log("hello");
-
-    $(".rating span").click(function(){
-       $(this).parent().children("span").removeClass("on");
+    $("#rating span").click(function(){
+        $(this).parent().children("span").removeClass("on");
         $(this).addClass("on").prevAll("span").addClass("on");
         return false;
     });
+
+    $("#createReviewBtn").click(function(){
+        $("#review_form").toggle();
+        $(this).hide();
+    });
+
 });
 
 
+ 
