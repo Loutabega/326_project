07922fd1c760f52ee5bde087c6f3db68b0ebf26b
austin <apbeermoney@gmail.com>
2019-12-08 03:20:00 -0500

    Added main page logic flow and scrape.js

--- view/js/main.js
+++ view/js/main.js
@@ -1,27 +1,24 @@
+//main / homepage js
+
+
 $(document).ready(function () {
     //Execute when search button is clicked
     $("#SearchButtonIcon").click(function(){
         
         //Retrieve the link in search bar
         var amazonLink = $("#searchBar").val();
-        //Trim and verify the link (make sure it is actual product)
 
-        //Save the link in session storage so we can use it in next page
-        // Save data to sessionStorage
+        //Trim input link
+        var pos = amazonLink.indexOf("?");
+        amazonLink = amazonLink.slice(0,pos);
+        
+        // *****TO DO*****
+        //Verify that input link is actual Amazon product
+
+        // Save url to sessionStorage so we can access it on next page
         sessionStorage.setItem('amazonLink', amazonLink);
-
-        // Get saved data from sessionStorage
 
         //Send user to next page --> product.html 
         $(location).attr('href', "product.html"); 
-
-
     });
 });
-
-    // When the searchbar search/submit button is pressed  
-    // $("#submitBtn").click(function() { 
-    //     //Retrieve the amazon link in the search bar                                                  
-    //     var amazonLink = $("#searchBar").val();
-    //     makeRequest(amazonLink);  
-    // });
--- view/js/scrape.js
+++ view/js/scrape.js
@@ -1,48 +1,31 @@
-//Make XMLHttpRequest to the passed in url using a proxy server
-// function makeRequest(url){
-//     var x = new XMLHttpRequest();
-//     x.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
-//     x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-//     x.onload = function() {
-//         alert(x.responseText);
-//     };
-//     x.send();
-// }
+//scrape.js
 
-
-//Make XMLHttpRequest using the url from the searchbar
-// $(document).ready(function() { 
-//     // When the searchbar search/submit button is pressed  
-//     $("#submitBtn").click(function() { 
-//         //Retrieve the amazon link in the search bar                                                  
-//         var amazonLink = $("#searchBar").val();
-//         makeRequest(amazonLink);  
-//     });
-// });
-
-
-
-const siteUrl = "https://www.amazon.com/dp/B01LWWY3E2/ref=cm_gf_aaam_iaaa_d_p0_c0_qd0_________________1Pjy485Yzh4K82Bo1GQb";
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+//Retrieve input link from main page
+const siteUrl = sessionStorage.getItem('amazonLink');
 
+// Load the Amazon HTML code as a string, which returns a Cheerio instance
 const fetchData = async () => {
   const result = await axios.get(siteUrl);
   return cheerio.load(result.data);
 };
 
-fetchData().then((value) => {
-    console.log(value);
-    // const title = value("#productTitle").text();
-    // const title = value("#productTitle").text();
 
-    // console.log(title);
-}).catch((reason) => {
+fetchData().then(($) => {
+  var scrapedData = {
+    "productTitle" : $("#productTitle").text().trim(),
+    "companyName" : $("#bylineInfo").text(),
+    "productImage": $('#landingImage').attr('src'),
+  };
+
+  // console.log(scrapedData.productTitle);
+  // console.log(scrapedData.companyName);
+
+  return scrapedData 
+
+})
+.catch((reason) => {
     console.log(reason);
 })
-
-
-
-// use trim
-//use
