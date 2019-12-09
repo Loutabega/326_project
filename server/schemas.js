2a55fea6c12f1adda2c29b781522ecaccc4e5436
Louis DePace <ldepace@umass.edu>
2019-12-09 00:06:33 -0500

    edited product page route to gather all info about company and product in order to render page

--- server/schemas.js
+++ server/schemas.js
@@ -3,7 +3,10 @@ const mongoose = require('./db')
 // Mongoose Schemas
 const CompanyArticleSchema = new mongoose.Schema({
     company: {type: String, required: true},
-    url: {type: String}
+    url: {type: String},
+    title: {type: String, default: "No Title Provided"},
+    published_date: {type: String, default: "Jan 1, 1642"},
+    excerpt: {type: String, default: "No excerpt Provided"}
 })
 
 const CompanyRatingSchema = new mongoose.Schema({
