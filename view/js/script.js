41cde472fee9701aa8f75f77a50a486330901667
Jiye Choi <jiyechoi@Jiyes-MacBook-Air.local>
2019-12-09 01:19:47 -0500

    updated_product.html

--- view/js/script.js
+++ view/js/script.js
@@ -12,30 +12,16 @@ $(document).ready(function () {
         $("#review_form").show();
     });
 
-    $("#check_review").change(function(){
-        if($(this).is(":checked")){
-
-        };
-        $("#review_form").show();
-
-    });
-
+    $("#cancel").click(function(){
+        $("#rating span").parent().children("span").removeClass("on");
+        $("#review_form").hide();
+    })
     $("#review_form").submit(function(){
         var stars = $("#rating span").parent().find(".star.on").length;
         var errorCount =0;
         var data = $("#rating_form").serializeObject();
         alert(data);
      });
-
-    $("#check_article").change(function(){
-        $("#article_form").toggle();
-    });    
-
-    $("#article_form").submit(function(){
-        var values =  $(this).serialize();
-         alert(values);
-     });
-
 });
 
 
